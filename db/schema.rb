a8475b10a331be33c3d09ffa40b33fd09c7ce61f
jaian3 <kentanakamura0816@gmail.com>
2019-11-26 21:50:01 +0900

    comment-form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_28_070634) do
+ActiveRecord::Schema.define(version: 2019_11_26_121929) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "user_name"
+    t.text "body"
+    t.integer "idea_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["idea_id"], name: "index_comments_on_idea_id"
+  end
 
   create_table "ideas", force: :cascade do |t|
     t.string "name"
